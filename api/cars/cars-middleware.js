94e328d4162f32e34ee23ca1f8bc6ca6503bbc49
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-06-08 16:55:55 -0400

    wip

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,17 +1,51 @@
+const vinValidator = require('vin-validator');
+
 const checkCarId = (req, res, next) => {
-  
+  if (req.params.id === undefined) {
+    res.status(404).json({
+      message: `car with id ${req.params.id} is not found`
+    })
+  } 
+    next()
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  if (!req.params.id) {
+  res.status(404).json({
+    message: `${req.params.id} is missing`
+    })
+  } else if (!req.body.vin) {
+    res.status(404).json({
+      message: `${req.body.vin} is missing`
+      })
+  } else if (!req.body.make) {
+    res.status(404).json({
+      message: `${req.body.make} is missing`
+      })
+  } else if (!req.body.model) {
+    res.status(404).json({
+      message: `${req.body.model} is missing`
+      })
+  } else if (!req.body.mileage) {
+    res.status(404).json({
+      message: `${req.body.mileage} is missing`
+      })
+  }
+  next()
 }
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+  if (!vinValidator.validate(req.body.vin)) {
+    res.status(400).json({ message: `vin ${req.body.vin} is invalid` })
+  }
+  next()
 }
 
 const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+  // if (!vinValidator.validate(req.body.vin)) {
+  //   res.status(400).json({ message: `vin ${req.body.vin} is invalid` })
+  // }
+  // next()
 }
 
 module.exports = {
--- api/server.js
+++ api/server.js
@@ -17,5 +17,13 @@ server.use('*', (req, res) => {
     })
 })
 
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      custom: 'something exploded inside the app',
+      message: err.message,
+      stack: err.stack,
+    })
+  });
+
 
 module.exports = server
